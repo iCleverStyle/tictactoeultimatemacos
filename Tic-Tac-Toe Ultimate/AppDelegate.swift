1cc317b820e1980e207b355f1aaed948f763a4de
Evgenii Kirillov <477504@gmail.com>
2025-02-27 20:00:22 +0400

    Initial Commit

---
+++ Tic-Tac-Toe Ultimate/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Tic-Tac-Toe Ultimate
+//
+//  Created by Евгений on 27.02.25.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
